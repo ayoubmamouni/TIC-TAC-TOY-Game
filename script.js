e3e7e16f11bc2f4efbb328994d041369ae9386e8
ayoub mamouni <47207888+ayoubmamouni@users.noreply.github.com>
2021-07-19 00:09:16 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,69 +1,59 @@
-const cards = Array.from(document.querySelectorAll('.card'))
+const cards = Array.from(document.querySelectorAll(".card"));
 const winner = [
-    [1, 2, 3],
-    [4, 5, 6],
-    [7, 8, 9],
-    [1, 5, 9],
-    [7, 8, 9],
-    [3, 5, 7],
-    [1, 4, 7],
-    [2, 5, 8],
-    [3, 6, 9]
-]
+  [1, 2, 3],
+  [4, 5, 6],
+  [7, 8, 9],
+  [1, 5, 9],
+  [7, 8, 9],
+  [3, 5, 7],
+  [1, 4, 7],
+  [2, 5, 8],
+  [3, 6, 9],
+];
 
-let [
-    firstPlayer,
-    secondPlayer,
-    count
-] = [
-    [],
-    [],
-    0
-]
+let [firstPlayer, secondPlayer, count] = [[], [], 0];
 
 function check(array) {
-    let finalResult = false;
-    for (let item of winner) {
-        let res = item.every(val => array.indexOf(val) !== -1)
-        if (res) {
-            finalResult = true;
-            break;
-        }
+  let finalResult = false;
+  for (let item of winner) {
+    let res = item.every((val) => array.indexOf(val) !== -1);
+    if (res) {
+      finalResult = true;
+      break;
     }
-    return finalResult;
+  }
+  return finalResult;
 }
 
 function winnerplayer(p) {
-    const modal = document.createElement('div')
-    const player = document.createTextNode(p)
-    const replay = document.createElement('button')
-
-    modal.classList.add('winner')
-    modal.appendChild(player)
-    replay.appendChild(document.createTextNode('Replay'))
-    replay.addEventListener('click', () => window.location.reload())
-    modal.appendChild(replay)
-    document.body.appendChild(modal)
+  const modal = document.createElement("div");
+  const player = document.createTextNode(p);
+  const replay = document.createElement("button");
+  modal.classList.add("winner");
+  modal.appendChild(player);
+  replay.appendChild(document.createTextNode("Replay"));
+  replay.onclick = () => window.location.reload();
+  modal.appendChild(replay);
+  document.body.appendChild(modal);
 }
 
 function draw() {
-    if (this.classList == 'card') {
-        count++
-        if (count % 2 !== 0) {
-            this.classList.add('x')
-            firstPlayer.push(Number(this.dataset.index))
-            if (check(firstPlayer)) {
-                winnerplayer('X is winner')
-            }
-        } else {
-            this.classList.add('o')
-            secondPlayer.push(Number(this.dataset.index))
-            if (check(secondPlayer)) {
-                winnerplayer('O is winner')
-            }
-        }
-        count === 9 && winnerplayer('Draw')
+  if (this.classList == "card") {
+    count++;
+    if (count % 2 !== 0) {
+      this.classList.add("x");
+      firstPlayer.push(Number(this.dataset.index));
+      if (check(firstPlayer)) {
+        winnerplayer("X is winner");
+      }
+    } else {
+      this.classList.add("o");
+      secondPlayer.push(Number(this.dataset.index));
+      if (check(secondPlayer)) {
+        winnerplayer("O is winner");
+      }
     }
-
+    count === 9 && winnerplayer("Draw");
+  }
 }
-cards.forEach(card => card.addEventListener('click', draw))
+cards.forEach((card) => card.addEventListener("click", draw));
